84420792b782b5f5134880d77c6d9cdce862382a
Zoe Oladokun <Zoeoladokun@gmail.com>
2024-04-06 14:20:34 +0100

    Update cookieclickers.py

--- cookieclickers.py
+++ cookieclickers.py
@@ -1,49 +1,103 @@
-from selenium import webdriver
+from seleniumwire import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import NoSuchElementException
 import time
+
+URL = "https://orteil.dashnet.org/cookieclicker/"
+PRODUCT_PRICE_PREFIX = "productPrice"
+PRODUCT_PREFIX = "product"
+
+
+def word_to_number(word):
+    number_dict = {
+        "million": 10 ** 6,
+        "billion": 10 ** 9,
+        "trillion": 10 ** 12,
+        "quadrillion": 10 ** 15,
+        "quintillion": 10 ** 18,
+        "sextillion": (10 ** 21),
+        "septillion": (10 ** 24),
+        "octillion": (10 ** 27),
+        "nonillion": (10 ** 30),
+        "decillion": (10 ** 33),
+        "undecillion": (10 ** 36),
+        "duodecillion": (10 ** 39),
+        "tredecillion": (10 ** 42),
+        "quattuordecillion": (10 ** 45),
+        "quindecillion": (10 ** 48),
+        "sexdecillion": (10 ** 51),
+        "septendecillion": (10 ** 54),
+        "octodecillion": (10 ** 57),
+        "novemdecillion": (10 ** 60),
+        "vigintillion": (10 ** 63),
+        # Add more prefixes as needed
+    }
+
+    if "ion" in word:
+        word.replace(",", "")
+    else:
+        for key, value in number_dict.items():
+            if key in word:
+                return int(word.replace(key, "").strip()) * value
+
+    return word
+
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+driver.get(URL)
 
-driver.get("https://orteil.dashnet.org/cookieclicker/")
-
-# https://orteil.dashnet.org/cookieclicker/
-
-cookie_id = "bigCookie"
-cookies_id = "cookies"
-product_price_prefix = "productPrice"
-product_prefix = "product"
-
-WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'English')]"))
+WebDriverWait(driver, 10).until(
+    ec.presence_of_element_located(
+        (By.CSS_SELECTOR, "#langSelect-EN"))
 )
 
-language = driver.find_element(By.XPATH, "//*[contains(text(), 'English')]")
-language.click()
+english = driver.find_element(By.CSS_SELECTOR, "#langSelect-EN")
+english.click()
 
-WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.ID, cookie_id))
+time.sleep(5)
+
+WebDriverWait(driver, 15).until(
+    ec.presence_of_element_located(
+        (By.CSS_SELECTOR, "#bigCookie"))
 )
 
-cookie = driver.find_element(By.ID, cookie_id)
+WebDriverWait(driver, 10).until(
+    ec.presence_of_element_located(
+        (By.ID, "cookies"))
+)
+
+cookie = driver.find_element(By.CSS_SELECTOR, "#bigCookie")
+time.sleep(3)
 
 while True:
     cookie.click()
-    cookies_count = driver.find_element(By.ID, cookies_id).text.split(" ")[0]
-    cookies_count = int(cookies_count.replace(",", ""))
-    
-    for i in range(4):
-        product_price = driver.find_element(By.ID, product_price_prefix + str(i)).text.replace(",", "")
+    cookie_number = driver.find_element(By.ID, "cookies")
+    num_text = cookie_number.text
+    modified_text = num_text.rsplit("0", 1)[0].rstrip()
+    num = int(''.join(filter(str.isdigit, modified_text)))
+    print(num)
 
-        if not product_price.isdigit():
+    for i in range(6):
+        try:
+            product_price = word_to_number(driver.find_element(By.ID, f"{PRODUCT_PRICE_PREFIX}{i}").text)
+
+            if not product_price.isdigit():
+                continue
+
+            product_price = int(product_price)
+
+            if num >= product_price:
+                WebDriverWait(driver, 10).until(
+                    ec.presence_of_element_located(
+                     (By.ID, f"{PRODUCT_PREFIX}{i}"))
+                )
+                time.sleep(1)
+                product = driver.find_element(By.ID, f"{PRODUCT_PREFIX}{i}")
+                product.click()
+        except NoSuchElementException:
             continue
-
-        product_price = int(product_price)
-
-        if cookies_count >= product_price:
-            product = driver.find_element(By.ID, product_prefix + str(i))
-            product.click()
-            break
