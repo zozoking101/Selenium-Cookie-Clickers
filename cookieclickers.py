ef8ee51e30a41a08beaf10c26495e7a761abc9dd
Zoe Oladokun <Zoeoladokun@gmail.com>
2024-04-06 15:02:40 +0100

    Update cookieclickers.py

--- cookieclickers.py
+++ cookieclickers.py
@@ -18,30 +18,30 @@ def word_to_number(word):
         "trillion": 10 ** 12,
         "quadrillion": 10 ** 15,
         "quintillion": 10 ** 18,
-        "sextillion": (10 ** 21),
-        "septillion": (10 ** 24),
-        "octillion": (10 ** 27),
-        "nonillion": (10 ** 30),
-        "decillion": (10 ** 33),
-        "undecillion": (10 ** 36),
-        "duodecillion": (10 ** 39),
-        "tredecillion": (10 ** 42),
-        "quattuordecillion": (10 ** 45),
-        "quindecillion": (10 ** 48),
-        "sexdecillion": (10 ** 51),
-        "septendecillion": (10 ** 54),
-        "octodecillion": (10 ** 57),
-        "novemdecillion": (10 ** 60),
-        "vigintillion": (10 ** 63),
+        "sextillion": 10 ** 21,
+        "septillion": 10 ** 24,
+        "octillion": 10 ** 27,
+        "nonillion": 10 ** 30,
+        "decillion": 10 ** 33,
+        "undecillion": 10 ** 36,
+        "duodecillion": 10 ** 39,
+        "tredecillion": 10 ** 42,
+        "quattuordecillion": 10 ** 45,
+        "quindecillion": 10 ** 48,
+        "sexdecillion": 10 ** 51,
+        "septendecillion": 0 ** 54,
+        "octodecillion": 10 ** 57,
+        "novemdecillion": 10 ** 60,
+        "vigintillion": 10 ** 63,
         # Add more prefixes as needed
     }
 
-    if "ion" in word:
-        word.replace(",", "")
+    if "ion" not in word:
+        word = word.replace(",", "")
     else:
         for key, value in number_dict.items():
             if key in word:
-                return int(word.replace(key, "").strip()) * value
+                return float(word.replace(key, "").strip()) * value
 
     return word
 
@@ -75,29 +75,36 @@ cookie = driver.find_element(By.CSS_SELECTOR, "#bigCookie")
 time.sleep(3)
 
 while True:
-    cookie.click()
-    cookie_number = driver.find_element(By.ID, "cookies")
-    num_text = cookie_number.text
-    modified_text = num_text.rsplit("0", 1)[0].rstrip()
-    num = int(''.join(filter(str.isdigit, modified_text)))
-    print(num)
+    try:
+        cookie.click()
+        cookie_number = driver.find_element(By.ID, "cookies")
+        num_text = cookie_number.text
+        if "cookies" in num_text:
+            num = int(word_to_number(num_text.split("cookies")[0]))
+            print(num)
+        else:
+            num = int(word_to_number(num_text.split("cookie")[0]))
+            print(num)
 
-    for i in range(21):
-        try:
-            product_price = word_to_number(driver.find_element(By.ID, f"{PRODUCT_PRICE_PREFIX}{i}").text)
+        for i in range(21):  # number of buildings + 1
+            try:
+                product_price = word_to_number(driver.find_element(By.ID, f"{PRODUCT_PRICE_PREFIX}{i}").text)
 
-            if not product_price.isdigit():
+                if not product_price.isdigit():
+                    continue
+
+                product_price = int(product_price)
+
+                if num >= product_price:
+                    WebDriverWait(driver, 10).until(
+                        ec.presence_of_element_located(
+                         (By.ID, f"{PRODUCT_PREFIX}{i}"))
+                    )
+                    time.sleep(1)
+                    product = driver.find_element(By.ID, f"{PRODUCT_PREFIX}{i}")
+                    product.click()
+            except NoSuchElementException:
                 continue
 
-            product_price = int(product_price)
-
-            if num >= product_price:
-                WebDriverWait(driver, 10).until(
-                    ec.presence_of_element_located(
-                     (By.ID, f"{PRODUCT_PREFIX}{i}"))
-                )
-                time.sleep(1)
-                product = driver.find_element(By.ID, f"{PRODUCT_PREFIX}{i}")
-                product.click()
-        except NoSuchElementException:
-            continue
+    except NoSuchElementException:
+        continue
